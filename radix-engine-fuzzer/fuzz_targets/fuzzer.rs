8ea2c98248f08fa239e97199fe7d90c9f3b91139
Bartosz Barwikowski <b.barwikowski@hacken.io>
2024-08-18 19:57:54 +0200

    added manifest_encode and manifest_decode

--- radix-engine-fuzzer/fuzz_targets/fuzzer.rs
+++ radix-engine-fuzzer/fuzz_targets/fuzzer.rs
@@ -1,8 +1,10 @@
 #![no_main]
 
+use std::io::Write;
+
 use libfuzzer_sys::fuzz_target;
 use radix_engine_common::dec;
-use scrypto::{address::Bech32Encoder, prelude::{FromPublicKey, NonFungibleGlobalId, RADIX_TOKEN}};
+use scrypto::{address::Bech32Encoder, prelude::{manifest_decode, manifest_encode, FromPublicKey, NonFungibleGlobalId, RADIX_TOKEN}};
 use scrypto_unit::TestRunner;
 use transaction::builder::ManifestBuilder;
 use utils::ContextualDisplay;
@@ -20,6 +22,16 @@ fuzz_target!(|data: &[u8]| {
         .create_proof_from_account_by_amount(account, RADIX_TOKEN, dec!("1"))
         .create_proof_from_account_by_amount(account, RADIX_TOKEN, dec!("1"))
         .build();
+
+    let encoded_manifest = manifest_encode(&manifest).unwrap();
+
+    // save encoded_manifest to file
+    let mut file = std::fs::File::create("encoded_manifest.bin").unwrap();
+    file.write_all(&encoded_manifest).unwrap();
+    // copy manually encoded_manifest.bin to corpus/fuzzer after ./run.sh
+
+    let manifest = manifest_decode(&encoded_manifest).unwrap();
+
     let receipt = test_runner.execute_manifest(
         manifest,
         vec![NonFungibleGlobalId::from_public_key(&public_key)],
